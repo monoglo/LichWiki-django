d10f20a658aa0875e1f6d39a6daacc4d441293f6
rankofmatrix <15032333203@163.com>
2020-04-14 15:50:03 +0800

    api less code

--- subjects/views.py
+++ subjects/views.py
@@ -9,37 +9,30 @@ from .models import Subject
 from .serializer import SubjectSerializer
 
 
-class SubjectList(APIView):
+class SubjectList(generics.ListCreateAPIView):
     """
     List all subjects, or create a new subject.
     """
-    def get(self, request, format=None):
-        subjects = Subject.objects.all()
-        serializer = SubjectSerializer(subjects, many=True, context={'request': request})
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = SubjectSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
 
 
 class SubjectDetail(APIView):
-
+    """
+        Get one subject, or update or delete a existed subject.
+    """
     def get_object(self, subject_name):
         try:
             return Subject.objects.get(s_name=subject_name)
         except Subject.DoesNotExist:
             raise Http404
 
-    def get(self, request, subject_name, format=None):
+    def get(self, request, subject_name):
         subject = self.get_object(subject_name)
         serializer = SubjectSerializer(subject, context={'request': request})
         return Response(serializer.data)
 
-    def put(self, request, subject_name, format=None):
+    def put(self, request, subject_name):
         subject = self.get_object(subject_name)
         serializer = SubjectSerializer(subject, data=request.data, context={'request': request})
         if serializer.is_valid():
@@ -47,7 +40,10 @@ class SubjectDetail(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, subject_name, format=None):
+    def delete(self, request, subject_name):
         subject = self.get_object(subject_name)
         subject.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
--- users/views.py
+++ users/views.py
@@ -3,40 +3,36 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import generics
 
 from .models import User
 from .serializer import UserSerializer
 
 
-class UserList(APIView):
-
-    def get(self, request, format=None):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True, context={'request': request})
-        return Response(serializer.data)
-
-    def post(self, request, format=None):
-        serializer = UserSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UserList(generics.ListCreateAPIView):
+    """
+        List all users, or create a new user.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
 class UserDetail(APIView):
-
+    """
+        Get one user, or update or delete a existed user.
+    """
     def get_object(self, user_name):
         try:
             return User.objects.get(u_name=user_name)
         except User.DoesNotExist:
             raise Http404
 
-    def get(self, request, user_name, format=None):
+    def get(self, request, user_name):
         user = self.get_object(user_name)
         serializer = UserSerializer(user, context={'request': request})
         return Response(serializer.data)
 
-    def put(self, request, user_name, format=None):
+    def put(self, request, user_name):
         user = self.get_object(user_name)
         serializer = UserSerializer(user, data=request.data, context={'request': request})
         if serializer.is_valid():
@@ -44,7 +40,7 @@ class UserDetail(APIView):
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, user_name, format=None):
+    def delete(self, request, user_name):
         user = self.get_object(user_name)
         user.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
