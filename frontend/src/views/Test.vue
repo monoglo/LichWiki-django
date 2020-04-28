2424d2832525b55f99cc161810be5a72d3745e6a
rankofmatrix <15032333203@163.com>
2020-04-28 13:59:45 +0800

    init vue frontend

---
+++ frontend/src/views/Test.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="about">
+      <h1>This is a test page.</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Test"
+    }
+</script>
+
+<style scoped>
+
+</style>
